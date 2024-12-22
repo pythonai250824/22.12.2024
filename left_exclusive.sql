f3c64fd123ea4f7a497d1151ba0ee7ba830d8efa
pythonai250824 <pythonai250824@gmail.com>
2024-12-22 21:07:14 +0200

    upload

--- left_exclusive.sql
+++ left_exclusive.sql
@@ -5,4 +5,5 @@
 -- where drivers.car_id IS NULL
 
 select drivers.*, cars.model as car_model, cars.color as car_color from drivers
-left outer join cars ON drivers.car_id = cars.car_id
+left JOIN cars ON drivers.car_id = cars.car_id
+where drivers.car_id is null
--- targil2.sql
+++ targil2.sql
@@ -1,10 +1,43 @@
 -- targil
 -- 1. use join- find all employees without branch
+select * from Employee
+left join Branch on Employee.branch_id = Branch.branch_id
+where Employee.branch_id is NULL;
+
 -- 1.5. use join- find all employees with and without branch
+select * from Employee
+left join Branch on Employee.branch_id = Branch.branch_id;
+
 -- 2. use join- find all branches without employees
--- 2.5. use join- find all branches without and without employees
+select * from Branch
+left join Employee on Branch.branch_id = Employee.branch_id
+where Employee.emp_id is NULL;
+
 -- 3. use join- find all employees and all branches
+select * from Employee
+FULL join Branch on Employee.branch_id = Branch.branch_id;
+
 -- 4. use join- show only  employees who have branches
--- 5. use join - show all emnployees that dont have branch and branches that dont have employees
--- group by - count how many employees in each branch. show only barnches who have employees
--- group by - show avg salary for each branch. show only barnches who have employees
+select * from Employee
+inner join Branch on Employee.branch_id = Branch.branch_id;
+
+-- 5. use join - show all employees that dont have branch 
+--    and branches that dont have employees
+select * from Employee
+FULL join Branch on Employee.branch_id = Branch.branch_id
+where Employee.branch_id is null;
+
+-- group by - count how many employees in each branch. 
+-- show only barnches who have employees
+select Branch.*, count(Employee.emp_id) as count from Branch
+left join Employee on Branch.branch_id = Employee.branch_id
+group by Branch.branch_id
+having count(Employee.emp_id) > 0;
+
+-- group by - show avg salary for each branch. 
+-- show only barnches who have employees
+select Branch.*, avg(Employee.salary) as avg_salary from Branch
+inner join Employee on Branch.branch_id = Employee.branch_id
+group by Branch.branch_id;
+
+
