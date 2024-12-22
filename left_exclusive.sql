21d260b6c0144da8dd67d88b322997ddac867c2b
pythonai250824 <pythonai250824@gmail.com>
2024-12-22 18:48:58 +0200

    upload

---
+++ left_exclusive.sql
@@ -0,0 +1,5 @@
+-- select * from cars;
+-- select * from drivers;
+select drivers.*, cars.model as car_model, cars.color as car_color from drivers
+left join cars ON drivers.car_id = cars.car_id
+where drivers.car_id IS NULL
